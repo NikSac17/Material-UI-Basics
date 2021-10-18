2b7d230bfc49553202867f19156fed4caf6d0046
Nikhil Sachan <nsachan1983@gmail.com>
2021-10-18 16:42:45 +0530

    added layout component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Note from "./pages/Note";
 import Create from "./pages/Create";
 import { createTheme, ThemeProvider } from "@material-ui/core";
+import Layout from "./components/Layout";
 
 const theme = createTheme({
   palette: {
@@ -16,14 +17,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Note />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Note />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
